321f6895e55687c4f9246c6282d8bf8fd4114a1e
Justin Thelukai <info@kiwitrain.com>
2025-04-24 16:56:20 -0700

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_24_235407) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_24_235604) do
   create_table "continuous_glucose_levels", force: :cascade do |t|
     t.integer "member_id", null: false
     t.integer "value"
@@ -19,6 +19,12 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_24_235407) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["member_id"], name: "index_continuous_glucose_levels_on_member_id"
+  end
+
+  create_table "members", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "continuous_glucose_levels", "members"
