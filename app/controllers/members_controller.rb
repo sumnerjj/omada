d37208be0b9f3615ebb9a1ee2ab49a6644cc1644
Justin Thelukai <info@kiwitrain.com>
2025-04-24 17:13:07 -0700

    Add some basic stats to members controller

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,2 +1,15 @@
 class MembersController < ApplicationController
-end
+    def dashboard
+      @member = Member.find(params[:id])
+      @glucose_levels = @member.continuous_glucose_levels.order(tested_at: :desc)
+  
+      @stats = {
+        count: @glucose_levels.count,
+        average: @glucose_levels.average(:value)&.round(1),
+        min: @glucose_levels.minimum(:value),
+        max: @glucose_levels.maximum(:value),
+        latest: @glucose_levels.first
+      }
+    end
+  end
+  
